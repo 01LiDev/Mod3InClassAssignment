deb5b9ca2923098426d938b9c54e9c4a99cc6f20
twigjl <liwindx15@gmail.com>
2023-02-15 13:09:55 -0500

    Finished 6.8 and 6.17

--- src/main/java/com/jldevelops/mod3inclass/Mod3inclass.java
+++ src/main/java/com/jldevelops/mod3inclass/Mod3inclass.java
@@ -4,18 +4,37 @@
 
 package com.jldevelops.mod3inclass;
 
+import java.util.Scanner;
+
 /**
  *
  * @author johnsonli
  */
 public class Mod3inclass {
-
+    
     public static void main(String[] args) {
+       Scanner kb = new Scanner(System.in);
        double charge1 = calculateCharges("Joe",12);
        double charge2 = calculateCharges("Mathew",2);
        double charge3 = calculateCharges("Amanda",16);
        double Total = charge1 +charge2 + charge3;
-       System.out.printf("Total Charges: %.2f$", Total ) ;
+       System.out.printf("Total Charges: %.2f$ \n", Total ) ;
+       Boolean isTrue = true;
+       while(isTrue){
+           try{
+               System.out.print("Enter a Number(input a non-number to exit): ");
+               int input = kb.nextInt();
+               if(isEven(input)){
+                   System.out.println(input + " is an even number");
+               }else{
+                   System.out.println(input + " is an odd number");
+               }
+           }catch(Exception e){
+               isTrue = false;
+           }
+           
+           
+       }
     }
     public static double calculateCharges(String customer, int hours){
         double baseCharge = 2.00;
@@ -32,5 +51,14 @@ public class Mod3inclass {
             System.out.println("Hello, "+ customer + " ,you are charged 2.00$ for parking for " + hours + " hours!\n" );
             return baseCharge;
         }
+    }
+    public static Boolean isEven(int num){
+        if(num % 2 == 0){
+            return true;
+        }else
+        {
+            return false;
+        }
+        
     }
 }
